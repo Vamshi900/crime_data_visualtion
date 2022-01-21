f697925f62b4fe8de28c7b1d7b439edde32a20f3
Vamshi900 <vamshigeek1@gmail.com>
2022-01-21 19:48:31 +0000

    5 tasks done

--- scatter.py
+++ scatter.py
@@ -1,14 +1,45 @@
 import plotly.express as px
 import pandas as pd
 
+
 df = pd.read_csv('asoiaf_nodes_prop.csv')
 # print(df)
-fig = px.scatter(df, x = 'degree', y = 'peel', color = 'diversity',
-                 size = 'pagerank', custom_data = ['name', 'diversity', 'pagerank'])
+fig = px.scatter(df,
+                 x = 'pagerank', # x axis
+                 y= 'betweenness', # y axis
+                 log_x=True, log_y=True, # log scale 
+                 color_continuous_scale='Turbo', # color scale 
+                 color = 'diversity',
+                 size = 'pagerank', 
+                 custom_data = ['name', 'degree','peel', 'diversity'], # custom data for tool tip
+                 labels=dict( betweenness="betweeness centrality"), # axis labels dictonary 
+                 )
 
+# tooltip data 
 fig.update_traces(hovertemplate = ( 'name: %{customdata[0]}<br>'
-                                    'degree: %{x}, peel: %{y}<br>'
+                                    'pagerank: %{x}, betweenness: %{y}<br>'
+                                    'degree: %{customdata[1]:.2e}<br>'
+                                    'peel: %{customdata[2]:.2e}'
                                     'diversity: %{customdata[1]:.2e}<br>'
-                                    'pagerank: %{customdata[2]:.2e}'))
+                                    ))
 
-fig.write_image("fig1.png")
+fig.update_layout(
+    xaxis=dict(
+          rangeselector=dict(
+            buttons=list([
+                dict(count=1,
+                     label="1y",
+                     step="year",
+                     stepmode="backward"),
+                dict(step="all")
+            ])
+        ),
+        rangeslider=dict(
+            visible=True
+        ),
+        type="log"
+    )
+)                                    
+config = dict({'scrollZoom': True})
+fig.show(config=config)
+fig.write_image("./fig1.png")
