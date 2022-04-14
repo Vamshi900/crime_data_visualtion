f4e0f82c3f3ea727a8cccc21fbe4046472ae3f6d
Vamshi900 <vamshigeek1@gmail.com>
2022-04-14 19:05:25 +0000

    fixed code changes

--- app/storyline.py
+++ app/storyline.py
@@ -2,6 +2,16 @@ from dash import html, dcc
 from navbar import Navbar
 from app import app, port
 import dash_bootstrap_components as dbc
+import pandas as pd
+
+from filters import FilterCreation
+
+# loading the data frame
+data_frame = pd.read_csv("processed_crimes_sample_5000.csv")
+
+# initializing the filter object for passed data frame
+filters = FilterCreation(data_frame)
+
 
 nav = Navbar()
 
@@ -14,38 +24,37 @@ buttons = html.Div([
     dbc.Button("Criminals on a holiday!!", color="secondary", className="me-1", style=dict(width=150,height=60)),
     dbc.Button("Careful on Saturdays!!", color="secondary", className="me-1", style=dict(width=150,height=60)),
     dbc.Button("Sensitive Crimes", color="secondary", className="me-1", style=dict(width=150,height=60)),
-    html.Br(),
-    html.Br(),
+    html.Br()
 ])
 
-chicago_pd_effect = html.div([
+chicago_pd_effect = html.Div([
     html.Div(
         className = "row", children = [
             html.Div(className="Element", children = [
                 html.Label("Select Years"),
                 filters.range_selector("years")
-            ], style=dict(width="30%",background="#F9F9F9",marginLeft="40px",padding="15px")),
-            html.Div(className="Element", children = [
-                html.Label("Select Crime Type"),
-                filters.dropdown_filter("crime_type")
-            ], style=dict(width="20%",background="#F9F9F9",padding="15px")),
+            ], style=dict(width="33.3%",marginLeft="20px",padding="15px")),
             html.Div(className="Element", id="filter_district", children = [
                 html.Label("Select Districts"),
                 filters.dropdown_filter("districts")
-            ], style=dict(width="20%",background="#F9F9F9",padding="15px")),
+            ], style=dict(width="33.3%",padding="15px")),
             html.Div(className="Element", children = [
                 html.Label("Select Month"),
                 filters.dropdown_filter("months")
-            ], style=dict(width="20%",background="#F9F9F9",padding="15px"))
+            ], style=dict(width="20%",padding="15px"))
         ], style=dict(display='flex',)
     ),
+    html.Div(
+        className = "EffectivePD", children = [
+            dcc.Graph(id="effective_pd", figure={})
+        ], style=dict(display='flex',)
+    )
 ])
 
 body = html.Div(
     className = "container scalable",
     children = [
         html.Br(),
-        html.H1("Our view on the Crime Story"),
         buttons,
         chicago_pd_effect
 ])
